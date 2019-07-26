20d919e31c055082c6789a6c379ab9192940a6c8
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-25 21:45:37 -0700

    Add custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,22 @@ class MyApp extends StatelessWidget {
               Text(
                 'Philip Yu',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
-              )
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ],
           ),
         ),
